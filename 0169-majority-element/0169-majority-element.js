02b249707d74517653419ec1d36fb8d8e20698d4
fathima naja <fathimanajamuneer@gmail.com>
2025-08-25 11:30:44 +0530

    Time: 3 ms (61.99%), Space: 59 MB (15.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -5,7 +5,6 @@
 var majorityElement = function(nums) {
      let n= nums.length;
     let maj=n/2
-    let count=0;
     let freq=new Map()
 for(let num of nums){
   freq.set(num,(freq.get(num)||0)+1)
